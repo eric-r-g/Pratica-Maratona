f1b3d5de6dcb89887b5c467e4b66a7011e476d0f
eric-r-g <eric.rodrig.gomes@gmail.com>
2024-12-09 13:29:32 -0300

    Update lexical.cpp

--- questões maratona /lexical.cpp
+++ questões maratona /lexical.cpp
@@ -1,52 +1,38 @@
 #include <stdio.h>
-
-void merge_sort(long l[], int tam){
-	if (tam == 1) return ; 
-	
-	int meio = tam / 2;
-	long l_a[meio], l_b[meio + 1];
-	
-	int tam_a = meio;
-	int tam_b = tam - tam_a;
-	
-	for (int i = 0; i < tam_a; ++i) l_a[i] = l[i];
-	for (int i = 0; i < tam_b; ++i) l_b[i] = l[i + tam_a];
-	
-	merge_sort(l_a, tam_a);
-	merge_sort(l_b, tam_b);
-	
-	int e, ou;
-	int i = tam_a - 1, j = tam_b - 1, p = tam - 1;
-	while(i > -1 && j > -1){
-		
-		e = l_a[i] & l_b[j];
-		ou = l_a[i] | l_b[j];
-		
-		if (l_a[i] > l_b[j])
-		{
-			l_a[i] = ou;
-			--j;
-		} 
-		else 
-		{
-			l_b[j] = ou;
-			--i;
-		}
-		l[p--] = e;
-	}
-	
-	for(;i > -1; --i)l[p--] = l_a[i];
-	for(;j > -1; --j)l[p--] = l_b[j];
-}
-
+#include <string.h>
 
 int main(){
-	int n;
-	long lista[100000]; 
+	int n, entr[100000];
 	scanf("%d", &n);
-	for(int i = 0; i < n; ++i) scanf("%ld", &lista[i]);
-	merge_sort(lista, n);
-	for(int i = 0; i < n; ++i) printf("%ld ", lista[i]);
+	for(int i = 0; i < n; ++i) scanf("%d", &entr[i]);
 	
+	// crio um vetor onde começa zero, e se o bit estiver ligado irei alocar 
+	// na posicão mais a esquerda, marcado pelo pos_bit;
+	int saida[100000], pos_bit[31];
+	memset(saida, 0, n*sizeof(int));
+	memset(pos_bit, 0, 31*sizeof(int)); 
+	// brincando com o memset, mas na pratica é igual a pos_bit[31] = {} e saida[100000] = {}
+	// (talvez um pouco mais rapido?)
+	
+	
+	// defino as potencias de 2 necessarias
+	long pot2[31];
+	pot2[0] = 1;
+	for(int i = 1; i < 31; ++i) pot2[i] = pot2[i - 1] * 2;
+
+	
+	// para cada um dos bits dos n numeros faco essa verificação
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < 31; ++j){
+			if (entr[i] & pot2[j]){
+				saida[pos_bit[j]] |= pot2[j];
+				++pos_bit[j];
+			}
+		}
+	}
+	
+	
+	for(int i = 0; i < n - 1; ++i) printf("%d ", saida[i]);
+	printf("%d\n", saida[n - 1]);
 	return 0;
 }
