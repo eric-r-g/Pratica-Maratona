b2f9896ff67e0d72559b2b9a9c8971530bbb5702
eric-r-g <eric.rodrig.gomes@gmail.com>
2025-07-06 21:58:18 -0300

    adicionado fenwick tree

---
+++ algoritmos famosos/Estruturas de dado/fenwick_tree.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> f_tree;
+vector <int> arr;
+int n;
+
+// retorna a soma do intervalor 0, k
+int prefixSum(int k){
+    ++k; // correção do inicio no indice 1 na f_tree
+    int soma = 0;
+    
+    for(int i = k; i > 0; i -= (i & -i))
+        soma += f_tree[i];
+
+    return soma;
+}
+
+// retorna a soma do intervalor ini, fim
+int rangeSum(int ini, int fim){
+    return prefixSum(fim) - prefixSum(ini - 1);
+}
+
+// atualizar a f_tree caso algum valor seja somado
+void update(int k, int val){
+    ++k;  // correção do inicio no indice 1 na f_tree
+    for(int i = k; i <= n; i += (i & -i))
+        f_tree[i] += val;
+
+    return;
+}
+
+void build(){
+    for(int i = 1; i <= n; ++i){
+        f_tree[i] += arr[i - 1];
+        int j = i + (i & -i);
+        if (j < n) 
+            f_tree[j] += arr[i - 1];
+    }
+
+    return;
+}
+
+int main(){
+    cin >> n;
+    f_tree.resize(n + 1);
+    arr.resize(n);
+    for(int i = 0; i < n; ++i){
+        cin >> arr[i];
+    }
+    // codigo restante do problema
+    return 0;
+}
